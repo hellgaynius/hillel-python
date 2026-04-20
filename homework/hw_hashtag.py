c8e6809c9d90397e8c3d2a26f97071434e14b581
Mariia Briukhanova <tnuocc@gmail.com>
2026-04-20 21:12:54 +0300

    add length limit for hashtag

--- homework/hw_hashtag.py
+++ homework/hw_hashtag.py
@@ -6,4 +6,4 @@ hashtag = user_input.title()
 for char in [*string.punctuation, ' ']:
     hashtag = hashtag.replace(char, '')
 
-print(f'#{hashtag}')
+print(f'#{hashtag[:139]}') # всього 140 символів з решіткою включно
