6ce21decaa499fffd8704026e775e5edac6d7127
Mariia Briukhanova <tnuocc@gmail.com>
2026-04-20 21:06:56 +0300

    add infinite calculator

--- homework/hw_calculator.py
+++ homework/hw_calculator.py
@@ -1,20 +1,21 @@
-print("Введіть перше число:")
-number_1 = int(input())
+def calculate_user_input():
+    print("Введіть перше число:")
+    number_1 = int(input())
 
-print("Введіть друге число:")
-number_2 = int(input())
+    print("Введіть друге число:")
+    number_2 = int(input())
 
-print("Введіть операцію (+, -, *, /):")
-operation = input()
+    print("Введіть операцію (+, -, *, /):")
+    operation = input()
 
-operations_map = {
-    "+": lambda a, b: a + b,
-    "-": lambda a, b: a - b,
-    "*": lambda a, b: a * b,
-    "/": lambda a, b: a / b,
-}
+    operations_map = {
+        "+": lambda a, b: a + b,
+        "-": lambda a, b: a - b,
+        "*": lambda a, b: a * b,
+        "/": lambda a, b: a / b,
+    }
 
-if number_2 == 0 and operation == "/":
-    print('No zero division in this universe')
-else:
-    print(operations_map[operation](number_1, number_2))
+    if number_2 == 0 and operation == "/":
+        print('No zero division in this universe')
+    else:
+        print(operations_map[operation](number_1, number_2))
